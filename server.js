95d4cd9126f5c019296c976bcafba3810482b007
陳德元 <r06521608@ntu.edu.tw>
2020-10-24 20:18:52 -0500

    add helmet

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 
+securitySetup = function(app) {
+  var connectSources, helmet, scriptSources, styleSources;
+  helmet = require("helmet");
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.noSniff());
+  app.use(helmet.crossdomain());
+  scriptSources = ["'self'", "'unsafe-inline'", "'unsafe-eval'", "ajax.googleapis.com"];
+  styleSources = ["'self'", "'unsafe-inline'", "ajax.googleapis.com"];
+  connectSources = ["'self'", "ws://localhost:3000"]
+  return app.use(helmet.contentSecurityPolicy({
+    defaultSrc: ["'self'"],
+    scriptSrc: scriptSources,
+    styleSrc: styleSources,
+    connectSrc: connectSources,
+    reportUri: '/report-violation',
+    reportOnly: false,
+    setAllHeaders: false,
+    safari5: false
+  }));
+};
+
 //將 express 放進 http 中開啟 Server 的 3000 port ，正確開啟後會在 console 中印出訊息
 const server = require('http').Server(app).listen(3000,()=>{console.log('open server!')})
 
